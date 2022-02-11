658ce0c8b9cb3534c2c0e3b107a3d135e76b31d0
herrjoseg <herrjoseg@gmail.com>
2022-02-10 22:21:51 -0500

    6-Commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
 */
 
 int main(void);
-printf("Size of char data type : %d\n", sizeof(char));
-printf("Size of int data type : %d\n", sizeof(int));
-printf("Size of int data type : %d\n", sizeof(long int));
-printf("Size of float data type : %d\n", sizeof(long lonh int));
-printf("Size of float data type : %d\n", sizeof(float));
+printf("Size of a char: %i byte(s)\n", sizeof(char));
+printf("Size of a int: %i byte(s)\n", sizeof(int));
+printf("Size of long int: %i byte(s)\n", sizeof(long int));
+printf("Size of long long int: %i byte(s)\n", sizeof(long lonh int));
+printf("Size of float: %i byte(s)\n", sizeof(float));
 return (0);
 }
