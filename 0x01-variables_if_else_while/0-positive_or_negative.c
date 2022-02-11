206e5b3f9030053fbb21bf6b7faa0679d3bccd88
herrjoseg <herrjoseg@gmail.com>
2022-02-11 17:30:37 -0500

    0-Commit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - Evaluate random number
+* Return: Always 0
+*/
+
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
+else
+printf("%d is positive\n", n);
+return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+**READMI Proyecto 0x01-variables_if_else_while**
+-0x01-variables_if_else_while = 0. Positive anything is better than negative nothing
