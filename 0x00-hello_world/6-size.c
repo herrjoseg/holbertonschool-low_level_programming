b5d553abfce7649d44e460ccc4819ade6b41110f
herrjoseg <herrjoseg@gmail.com>
2022-02-10 22:31:03 -0500

    6-Commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-
 /**
- * main - main function principal
- * Return: sucessfull 0.
+ * main - main function
+ * Return: Always 0.
 */
-
-int main(void);
+int main(void)
+{
 printf("Size of a char: %i byte(s)\n", sizeof(char));
-printf("Size of a int: %i byte(s)\n", sizeof(int));
-printf("Size of long int: %i byte(s)\n", sizeof(long int));
-printf("Size of long long int: %i byte(s)\n", sizeof(long lonh int));
-printf("Size of float: %i byte(s)\n", sizeof(float));
+printf("Size of an int: %i byte(s)\n", sizeof(int));
+printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+printf("Size of a float: %i byte(s)\n", sizeof(float));
 return (0);
 }
