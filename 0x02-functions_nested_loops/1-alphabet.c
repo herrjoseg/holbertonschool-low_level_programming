d7f4c6cf1fdc13ed4dc1810c792aba65d5c501d2
herrjoseg <herrjoseg@gmail.com>
2022-02-15 17:06:00 -0500

    2-Commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_alphabet - Imprime "alfabeto en minuscula"
+*/
+
+void print_alphabet(void)
+{
+
+int i;
+
+for(i = 97; i <= 122; i++)
+{
+_putchar(i);
+ }
+_putchar('\n');
+}
