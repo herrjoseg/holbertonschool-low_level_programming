fdae0c691a7f0caaf6ad1d67b81a2f3d8251f72d
herrjoseg <herrjoseg@gmail.com>
2022-02-15 19:00:22 -0500

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_sign - checks for alphabetic character.
+* @c: Caracter a chek
+* Return: 1 if c is a letter, lowercase or uppercase
+*/
+
+int print_sign(int c)
+{
+
+if (c >= 0)
+return (1);
+else if (c <= 0)
+return (-1);
+else
+return (0);
+}
