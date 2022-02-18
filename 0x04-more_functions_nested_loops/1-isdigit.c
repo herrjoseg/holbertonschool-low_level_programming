7c1b6b17d3d8aea6a95ebd600a77e89fdd9b89fc
herrjoseg <herrjoseg@gmail.com>
2022-02-17 22:07:35 -0500

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _isdigit - Es un digito.
+* @c: Caracter a chek
+* Return: 1 if c is lowercase or 0 otherwise
+*/
+
+int _isdigit(int c)
+{
+
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
